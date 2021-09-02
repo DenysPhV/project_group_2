b7f9edee90a2b434f54e95322f5ffffc6303aed7
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-02 11:15:00 +0300

    сборщик готов к бою

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+// ============== Импорт на pnotify ===================
+// import '@pnotify/core/dist/BrightTheme.css';
+// import '@pnotify/core/dist/PNotify.css';
+// import { error } from '@pnotify/core';
+
+// ================= debounce ==========================
+// const debounce = require('lodash.debounce');

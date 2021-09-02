81de26a198e23c8bbe84306b51a882bf7968ee8f
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-02 16:06:06 +0300

    footer-container

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './sass/main.scss';
+import './sass/main.scss';
 // ============== Импорт на pnotify ===================
 // import '@pnotify/core/dist/BrightTheme.css';
 // import '@pnotify/core/dist/PNotify.css';

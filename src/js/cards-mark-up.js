6f9fc08f58a7e469ac2fa6747e8cdb31be6061b7
Dmytro Salii <dmitriysalii@gmail.com>
2021-09-03 21:40:52 +0300

    обновил ренер карточки и темплейт

--- src/js/cards-mark-up.js
+++ src/js/cards-mark-up.js
@@ -6,11 +6,25 @@ const apiService = new ApiService();
 
 
 
+// apiService.fetchTrending().then(cards => {
+//     const newCards = cards.map(card => card.release_date = card.release_date.substring(0, 4))
+//     cardsMarkUp(cards);
+// });
+
+// apiService.fetchTrending().then(cards => {
+//     cards.map(card => card.release_date = card.release_date.substring(0, 4));
+//    return cardsMarkUp(cards);
+// });
+
+
 apiService.fetchTrending().then(cards => {
-    cardsMarkUp(cards)
-}); 
+    cards.map(card => card.release_date = card.release_date.substring(0, 4));
+    cardsMarkUp(cards);
+});
+
 
 
 function cardsMarkUp (cards) {
     refs.galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(cards));
 }
+

734545074b20e9ffde97fe29470666974f48ac3a
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-04 12:30:15 +0300

    Правки

--- src/js/cards-mark-up.js
+++ src/js/cards-mark-up.js
@@ -1,13 +1,15 @@
 import refs from './refs.js';
 import cardsTemplate from '../templates/cards.hbs';
 import ApiService from './apiService';
+import modalFilmBox from './modal-film';
 
 const apiService = new ApiService();
 
 
 
 apiService.fetchTrending().then(cards => {
-    cardsMarkUp(cards)
+    cardsMarkUp(cards);
+    modalFilmBox();
 }); 
 
 

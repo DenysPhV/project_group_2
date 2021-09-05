f3dbd4d8d8b6aab1e7e15e2c6310d4f1f6c21f07
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-05 23:48:59 +0300

    проблема

--- src/js/cards-mark-up.js
+++ src/js/cards-mark-up.js
@@ -1,20 +1,26 @@
 import { galleryContainer } from './refs.js';
 import cardsTemplate from '../templates/cards.hbs';
-import ApiService from './apiService';
+// import ApiService from './apiService';
 import modalFilmBox from './modal-film';
 import currentMovies from './currentMovies.js';
 
-const apiService = new ApiService();
+// const apiService = new ApiService();
 
-apiService.fetchTrending(1).then(cards => {
-  cards.results.map(card => (card.release_date = card.release_date.substring(0, 4)));
-  cardsMarkUp(cards.results);
+// apiService.fetchTrending(1).then(cards => {
+//   cards.results.map(card => (card.release_date = card.release_date.substring(0, 4)));
+//   cardsMarkUp(cards.results);
 
+//   modalFilmBox();
+
+//   currentMovies.movies = cards.results;
+// });
+// function renderGallery(data) {
+//   galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(data));
+// }
+export function cardsMarkUp(cards) {
+  cards.map(card => (card.release_date = card.release_date.substring(0, 4)));
+  galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(cards));
   modalFilmBox();
 
-  currentMovies.movies = cards.results;
-});
-
-export function cardsMarkUp(cards) {
-  galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(cards));
+  currentMovies.movies = cards;
 }

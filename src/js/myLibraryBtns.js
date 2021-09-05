fe1185e2883ed40505e2863b01826048b99c6a99
KMyroslav <kukhtarukm@gmail.com>
2021-09-05 15:35:36 +0300

    Adds My Library btns logic

---
+++ src/js/myLibraryBtns.js
@@ -0,0 +1,22 @@
+// Цей скрипт відповідає за добавленя фільмів з перглянутих або тих що в черзі на сторінку
+
+import { galleryContainer } from './refs.js';
+import cardsTemplate from '../templates/cards.hbs';
+import currentMovies from './currentMovies.js';
+
+export { onWatchedBtnClick, onQueueBtnClick };
+
+function onWatchedBtnClick() {
+  const cards = JSON.parse(localStorage.getItem('Watched'));
+  createCards(cards);
+}
+
+function onQueueBtnClick() {
+  const cards = JSON.parse(localStorage.getItem('Queued'));
+  createCards(cards);
+}
+
+function createCards(cards) {
+  galleryContainer.innerHTML = cardsTemplate(cards);
+  currentMovies.movies = cards;
+}

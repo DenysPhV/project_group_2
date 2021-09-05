7a08126db547854bffb8c2b3946cd0604d9bfeee
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-09-05 03:04:10 +0300

    добавляет стили кнопок header-а библиотеки (Watched и Queue), исправляет стили формы запросов

--- src/js/homeBtnRenderPage.js
+++ src/js/homeBtnRenderPage.js
@@ -1,36 +1,36 @@
 // Скрипт отвечает за отрисовку главной страницы по клику на кнопку HOME и ЛОГО
 
 import ApiService from './apiService';
-import { homeBtnEl, logoEl, libBtnEl, headerEl, inputWrapEl } from './refs.js';
+import { homeBtnEl, logoEl, libBtnEl, headerEl, searchForm, galleryContainer } from './refs.js';
 import { cardsMarkUp } from './cards-mark-up';
 import { renderLibHeaderOnClick } from './libBtnRenderHeader';
 
 const apiService = new ApiService();
 
 export const renderMainPageOnClick = e => {
+  galleryContainer.innerHTML = '';
   e.preventDefault();
   homeBtnEl.classList.add('nav-menu__btn_active');
   libBtnEl.classList.remove('nav-menu__btn_active');
   headerEl.classList.replace('header__lib-bg-js', 'header__main-bg-js');
-  inputWrapEl.classList.add('input-wrap_searchIcon');
-  homeBtnEl.style.color = '#ffffff';
-  apiService.resetPage();
+  searchForm.classList.add('input-wrap_searchIcon');
+
+  libBtnEl.classList.add('nav-menu__btn_hover');
+
   apiService
-    .fetchTrending()
+    .fetchTrending(1)
     .then(data => {
-      console.log(data);
-      return data;
+      return data.results;
     })
     .then(cardsMarkUp);
 
   homeBtnEl.removeEventListener('click', renderMainPageOnClick);
-  logoEl.removeEventListener('click', renderMainPageOnClick);
   libBtnEl.addEventListener('click', renderLibHeaderOnClick);
 };
 
 export const renderInputOnClick = () => {
   const inputMarkup = '<input class="header__input" type="text" placeholder="Search movies" />';
-  inputWrapEl.innerHTML = inputMarkup;
+  searchForm.innerHTML = inputMarkup;
   homeBtnEl.removeEventListener('click', renderInputOnClick);
   logoEl.removeEventListener('click', renderInputOnClick);
 };
@@ -39,10 +39,5 @@ const resetDefaultSet = e => {
   e.preventDefault();
 };
 
-const remoldColor = () => {
-  homeBtnEl.style.color = '#ffffff';
-};
-
 logoEl.addEventListener('click', resetDefaultSet);
-homeBtnEl.addEventListener('click', remoldColor);
-libBtnEl.addEventListener('click', renderLibHeaderOnClick);
+logoEl.addEventListener('click', renderMainPageOnClick);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,10 +1,9 @@
-
 export const galleryContainer = document.querySelector('.js-gallery');
 export const homeBtnEl = document.querySelector('.home-btn-js');
 export const logoEl = document.querySelector('.logo-js');
 export const libBtnEl = document.querySelector('.lib-btn-js');
 export const headerEl = document.querySelector('.header');
-export const inputWrapEl = document.querySelector('.input-wrap');
+// export const inputWrapEl = document.querySelector('.input-wrap');
 export const inputText = document.querySelector('.header__input');
 export const searchForm = document.querySelector('.input-wrap');
 export const notification = document.querySelector('.notification');

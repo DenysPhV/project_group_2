682178b5aee73aa36a41dc3e000de6b3ca815a91
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-05 17:05:43 +0300

    modal-images

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,47 +2,56 @@ const arrFooterDev = [
   {
     name: 'Andrii Lypovetskyi',
     position: 'Developer',
+    preview: '../src/images/pictures/thor.jpg',
   },
   {
     name: 'Aleksandr Bondarenko',
     position: 'Developer',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'DmytroMS',
     position: 'Developer',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'Iliya Lunev',
     position: 'Scrum master',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'KMyroslav',
     position: 'Developer',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'Masha Shytykova',
     position: 'Developer',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'Oleksandr Boiko',
     position: 'Developer',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'Ruslan Kuzma',
     position: 'Developer',
+    preview: './images/pictures/thor.jpg',
   },
   {
     name: 'Denys Filichkin',
     position: 'Team lead',
+    preview: './images/pictures/thor.jpg',
   },
 ];
 
 const itemFooterDev = document.querySelector('.footer__page-list');
-arrFooterDev.map(({ name, position }, index) => {
+arrFooterDev.map(({ name, position, preview }, index) => {
   itemFooterDev.insertAdjacentHTML(
     'afterbegin',
     `<li class="footer__page-item">
-     
+    <img class="footer__page-images" src="${preview}" alt="${name}" width="200px">
      <h3>${name}</h3>
      <p>${position}</p>
      </li>
@@ -51,7 +60,6 @@ arrFooterDev.map(({ name, position }, index) => {
 });
 
 // console.log(itemFooterDev);
-
 (() => {
   const refs = {
     openFooterBtn: document.querySelector('[data-footer-button]'),
@@ -59,18 +67,17 @@ arrFooterDev.map(({ name, position }, index) => {
     footer: document.querySelector('[data-footer]'),
   };
 
+  refs.openFooterBtn.addEventListener('click', toggleModal);
+  refs.closeFooterBtn.addEventListener('click', toggleModal);
+
   // закрыть по клику на ескейп
-  function pressKey(event) {
-    if (event.key === 'Escape') {
-      refs.footer.classList.remove('is-hidden');
-    }
-  }
+  // function pressKey(event) {
+  //   if (event.key === 'Escape') {
+  //     refs.footer.classList.remove('is-hidden');
+  //   }
+  // }
 
   function toggleModal() {
     refs.footer.classList.toggle('is-hidden');
   }
-
-  refs.openFooterBtn.addEventListener('click', toggleModal);
-  refs.closeFooterBtn.addEventListener('click', toggleModal);
-  refs.closeFooterBtn.removeEventListener('keydown', pressKey);
 })();

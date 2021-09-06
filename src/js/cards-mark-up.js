bad172007a06fda2ca21e2e234f24ccde883c480
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-06 12:16:47 +0300

    Update cards-mark-up.js

--- src/js/cards-mark-up.js
+++ src/js/cards-mark-up.js
@@ -4,20 +4,6 @@ import ApiService from './apiService';
 import modalFilmBox from './modal-film';
 import currentMovies from './currentMovies.js';
 
-// const apiService = new ApiService();
-
-// apiService.fetchTrending(1).then(cards => {
-//   cards.results.map(card => (card.release_date = card.release_date.substring(0, 4)));
-//   cardsMarkUp(cards.results);
-
-//   modalFilmBox();
-
-//   currentMovies.movies = cards.results;
-// });
-// function renderGallery(data) {
-//   galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(data));
-// }
-
 export function cardsMarkUp(cards) {
   const apiService = new ApiService();
   // Запрос списка жанров

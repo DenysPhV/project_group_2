e91d67e1c247410e76591195ba38d7dea3dfc2f4
Andrii Lypovetskyi <li_andr@ukr.net>
2021-09-06 11:59:54 +0300

    жанры

--- src/js/cards-mark-up.js
+++ src/js/cards-mark-up.js
@@ -1,6 +1,6 @@
 import { galleryContainer } from './refs.js';
 import cardsTemplate from '../templates/cards.hbs';
-// import ApiService from './apiService';
+import ApiService from './apiService';
 import modalFilmBox from './modal-film';
 import currentMovies from './currentMovies.js';
 
@@ -17,10 +17,32 @@ import currentMovies from './currentMovies.js';
 // function renderGallery(data) {
 //   galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(data));
 // }
+
 export function cardsMarkUp(cards) {
-  cards.map(card => (card.release_date = card.release_date.substring(0, 4)));
+  const apiService = new ApiService();
+
+  apiService.fetchGenre().then(genres => {
+    // console.log(genres);
+    localStorage.setItem('genres', JSON.stringify(genres));
+  });
+  // Читаем из локал сторадж
+  const savedSettings = localStorage.getItem('genres');
+  const parsedSettings = JSON.parse(savedSettings);
+  console.log(cards);
+  cards.forEach((card, i) => {
+    card.release_date = card.release_date.substring(0, 4);
+    // Обрезаем жанры
+    if (card.genre_ids.length > 3) {
+      card.genre_ids = card.genre_ids.slice(0, 3);
+    }
+
+    card.genre_ids.forEach((genre, index) => {
+      parsedSettings.forEach(genrCard => {
+        if (genrCard.id === genre) card.genre_ids[index] = genrCard.name;
+      });
+    });
+  });
   galleryContainer.insertAdjacentHTML('beforeend', cardsTemplate(cards));
   modalFilmBox();
-
   currentMovies.movies = cards;
 }

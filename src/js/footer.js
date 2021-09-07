7fa52855ca78a7f05d5947e642a1faae605b6d5e
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-08 02:07:44 +0300

    current-fixed

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,51 +3,69 @@ const arrFooterDev = [
     name: 'Andrii Lypovetskyi',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U0209L32VGE-4d63a7b43c4f-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Aleksandr Bondarenko',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U020RDMUW20-57b717892067-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Dmytro Salii',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U020ALA32Q5-442b8c87d6d3-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Iliya Lunev',
     position: 'Scrum master',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U020KTB155J-6d17af4c8085-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Myroslav Kuhtaruk',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U020L0YFVPE-b8d72596946f-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Masha Shytykova',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U020SD6GU1F-5b19e948dcad-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Oleksandr Boiko',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U0210K7SHR7-e237547f4c4f-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Ruslan Kuzma',
     position: 'Full-Stack developer',
     preview: 'https://ca.slack-edge.com/T01E40QL2LD-U01KVFMHD5M-d78aac37dda3-512',
+    linkedin: '',
+    github: '',
   },
   {
     name: 'Denys Filichkin',
     position: 'Team lead',
     preview: 'https://ca.slack-edge.com/T01UW5D5RDJ-U020BQ51HE1-5e9ad54fc99b-512',
+    linkedin: '',
+    github: '',
   },
 ];
 
 const itemFooterDev = document.querySelector('.footer-modal__list');
-arrFooterDev.map(({ name, position, preview }, index) => {
+arrFooterDev.map(({ name, position, preview, linkedin, github }, index) => {
   itemFooterDev.insertAdjacentHTML(
     'afterbegin',
     `<li class="footer-modal__item">
@@ -56,6 +74,23 @@ arrFooterDev.map(({ name, position, preview }, index) => {
     <div class="footer-modal__desc">
      <h3>${name}</h3>
      <p>${position}</p>
+      <ul class="footer-modal__list-link">
+        <li class="footer-modal__item-link">
+      <a href="${github}" class="footer-modal__icon-link">
+          <svg class="footer-modal__icon" width="32px"
+    height="32px">
+           <use href="./images/sprite.svg#github"></use>
+         </svg>
+      </a>
+      </li>
+     <li class="footer-modal__item-link">
+      <a href="${linkedin}" class="footer-modal__icon-link">
+        <svg class="footer-modal__icon" width="32px"
+    height="32px">
+          <use href="./images/sprite.svg#linkedin"></use>
+        </svg>
+      </a>
+      </li></ul>
      </div>
      </li>
   `,

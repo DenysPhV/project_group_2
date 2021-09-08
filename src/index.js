9aeaabe1d928569921adf19894a6c3ddbf8bde5a
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-08 19:22:06 +0300

    to fixaed errore console

--- src/index.js
+++ src/index.js
@@ -8,18 +8,6 @@ import './js/renderBtnLibr';
 
 import './js/theme';
 
-// ============== Импорт на pnotify ===================
-// import '@pnotify/core/dist/BrightTheme.css';
-// import '@pnotify/core/dist/PNotify.css';
-// import { error } from '@pnotify/core';
-
-// ================= debounce ==========================
-// const debounce = require('lodash.debounce');
-
-// import ApiService from './js/apiService';
-
-// const apiService = new ApiService();
-
 import homeBtnRenderPage from './js/homeBtnRenderPage';
 
 import cardsTemplate from './templates/cards.hbs';

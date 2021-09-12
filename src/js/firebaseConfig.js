bd1a376b61c3b22d5f925d3953b7acbaf0e7ce77
Oleksandr Boiko <oleksandrboi56@gmail.com>
2021-09-12 08:43:43 +0300

    firebase v1.1

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -5,4 +5,5 @@ export const firebaseConfig = {
     storageBucket: 'filmoteka-g2.appspot.com',
     messagingSenderId: '216052405678',
     appId: '1:216052405678:web:ec2218605ec32f2d70243f',
+    databaseURL: "https://filmoteka-g2-default-rtdb.europe-west1.firebasedatabase.app/",
   };

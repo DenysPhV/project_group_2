48b5577382aad50836b8d502c2b11481c52557a0
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-14 09:48:57 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,21 +8,21 @@ export default class ApiService {
   // это fetch для запроса популярных фильмов  на главную страницу
   fetchTrending(page) {
     return fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`)
-      .then(r => r.json())
-      .then(data => {
+      .then((r) => r.json())
+      .then((data) => {
         return data;
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }
 
   //это fetch для запроса детальной инфо о фильме
   fetchMovieDetails(movieId) {
     return fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`)
-      .then(r => r.json())
-      .then(data => {
+      .then((r) => r.json())
+      .then((data) => {
         return data;
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }
 
   //это fetch для поиска фильмов по названию
@@ -30,35 +30,34 @@ export default class ApiService {
     return fetch(
       `${BASE_URL}/search/movie?api_key=${API_KEY}&page=${page}&query=${this.searchQuery}`,
     )
-      .then(r => r.json())
-      .then(data => {
+      .then((r) => r.json())
+      .then((data) => {
         return data;
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }
   //это fetch для загрузки жанров
   fetchGenre() {
     return fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`)
-      .then(r => r.json())
-      .then(data => {
+      .then((r) => r.json())
+      .then((data) => {
         return data.genres;
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }
 
- //это fetch для поиска видео трейлеров для фильмов
-// https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
-  
+  //это fetch для поиска видео трейлеров для фильмов
+  // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
+
   fetchVideo(movie_id) {
     return fetch(`${BASE_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`)
-      .then(r => r.json())
-      .then(data => {
-       // console.log("data_id", data);
+      .then((r) => r.json())
+      .then((data) => {
+        // console.log("data_id", data);
         return data.results;
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }
-
 
   get query() {
     return this.searchQuery;
